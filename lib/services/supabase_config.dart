90375b4ff588f5f24287406540bbd47e7f47036d
S.Niththilan <99943148+niththilan@users.noreply.github.com>
2025-07-23 14:52:57 +0530

    rdg

--- lib/services/supabase_config.dart
+++ lib/services/supabase_config.dart
@@ -7,13 +7,13 @@ class SupabaseConfig {
 
   // Google OAuth Configuration
   static const String googleWebClientId =
-      '631111437135-bvvu4b15elvmctbclkbmag856kke0nmq.apps.googleusercontent.com';
+      '631111437135-iuippmjn73ur1g4thacjmr5lq3k315t0.apps.googleusercontent.com';
   static const String googleWebClientSecret =
-      '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';
+      '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';
   static const String googleAndroidClientId =
-      '631111437135-234lcguj55v09qd7415e7ohr2p55b58j.apps.googleusercontent.com';
+      '631111437135-76ojbi40r925em3sinj9igoel5f4do1i.apps.googleusercontent.com';
   static const String googleIOSClientId =
-      '631111437135-jg42a9hahfchrrfhva4mbb0bddaq5g5f.apps.googleusercontent.com'; // OAuth Callback URLs - these MUST match your Google Cloud Console settings
+      '631111437135-5iajfi8mlc0olt9bla8tqhic6sior22j.apps.googleusercontent.com'; // OAuth Callback URLs - these MUST match your Google Cloud Console settings
   static const String oauthCallbackUrl =
       'https://xxasezacvotitccxnpaa.supabase.co/auth/v1/callback';
 
